08b57e831b87ba6ca4c11fcb8af64bd0e7008a77
Martin Nuñez <martinalejandronuniez@gmail.com>
2025-05-22 21:11:50 -0300

    se agrega route en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useEffect, useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Home from './components/Home'
-
-import './App.css'
-import { Container, Row, Col, Card } from 'react-bootstrap'
+// App.jsx
+import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Home from './components/Home';
+// Importá más componentes si tenés más rutas
+// import Ofertas from './components/Ofertas';
+// import Infaltables from './components/Infaltables';
+// import Administracion from './components/Administracion';
 
 function App() {
-  return(
-    
-    <Home></Home>
-  )
-  
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/ofertas" element={<Ofertas />} />
+        <Route path="/infaltables" element={<Infaltables />} />
+        <Route path="/administracion" element={<Administracion />} /> */}
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" className="mb-4">
+      <Container>       
+        <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+          <img
+            src="https://via.placeholder.com/40" 
+            alt=""
+            className="d-inline-block align-top me-2"
+          />
+          <span>Alebourg</span>
+        </Navbar.Brand>
+
+        <Nav className="ms-auto align-items-center">
+          <Nav.Link as={Link} to="/" className="me-3">Home</Nav.Link>
+          <Nav.Link as={Link} to="/" className="me-3">Productos Ganadores</Nav.Link>
+          <Nav.Link as={Link} to="/" className="me-3">Ofertas</Nav.Link>
+
+          <div className="d-flex align-items-center">
+            <Button variant="outline-light" as={Link} to="/" className="me-2">
+              Administración
+            </Button>
+            <Link to="/" className="text-white">
+              <FontAwesomeIcon icon={faShoppingCart} size="lg" />
+            </Link>
+          </div>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
