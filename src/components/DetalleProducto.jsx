a5f702b17242f19fd84edb62ca94eb77b343554c
Martin Nuñez <martinalejandronuniez@gmail.com>
2025-06-04 04:43:46 -0300

    mejora

--- src/components/DetalleProducto.jsx
+++ src/components/DetalleProducto.jsx
@@ -15,7 +15,7 @@ const DetalleProducto = () => {
   useEffect(() => {
     const fetchProducto = async () => {
       try {
-        const response = await fetch('https://raw.githubusercontent.com/martinalejandronuniezcursor2/alebourgprueba/refs/heads/main/public/productosalebourgactulizados.json');
+        const response = await fetch('https://raw.githubusercontent.com/martuargento/Alebourg/refs/heads/main/public/productosalebourgactulizados.json');
         const data = await response.json();
         const productoEncontrado = data.find(p => p.id === parseInt(id));
         
