b763a22a58d77cb333fee4feda0a7eaf93c68062
Martin Alejandro Nuñez <martinalejandronuniez@gmail.com>
2025-11-03 04:18:10 -0300

    se agregan metricas en modo admin 2

--- api/analytics/track.js
+++ api/analytics/track.js
@@ -1,6 +1,15 @@
 import { getSupabaseAdminClient } from '../_supabaseClient.js';
 
 export default async function handler(req, res) {
+  // CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
